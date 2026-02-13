c90d4c927564cfd314673d7c6bf9d5ae3cddebec
justinsly <othman09omar@gmail.com>
2026-02-13 15:09:17 +0100

    adds a basic health system for the base enemy scene, adjusts some if conditions

--- scenes/entities/enemies/enemy.gd
+++ scenes/entities/enemies/enemy.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
-@export var sceneboom: PackedScene ## explosion effect scene
-@export var sceneheal: PackedScene ## the healing item scene to be dropped
+@export var health: int = 1 ## the starting health for the enemy
+@export var movement_speed: int = 250 ## do i need to write a description for this
 @export var bounce_on_hit: bool ## determines if the enemy should move backward a little bit upon hitting the player
 @export var drop_heal_item_on_death: bool ## determines if the enemy should drop the healing item upon being destroyed
+@export var boom_speed_mult: float = 1.5 ## the multiplier to apply to the explosion effect's speed
+@export var sceneboom: PackedScene ## explosion effect scene
+@export var sceneheal: PackedScene ## the healing item scene to be dropped
 
 var speed = 250
 
@@ -14,20 +17,28 @@ func initialize(startposx: float, startposy: float):
 	position.x = startposx
 	position.y = startposy
 
+func take_damage(dmg: int = 1):
+	health -= dmg
+	if health <= 0:
+		explode()
+	else:
+		take_knockback()
+
+func take_knockback():
+	var delta = get_process_delta_time() 
+	$KnockTimer.start()
+	set_process(false)
+	while $KnockTimer.time_left > 0:
+		if !get_tree().paused:
+			position.y -= 200 * delta
+		await get_tree().create_timer(0.01 * delta).timeout
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_area_entered(area):
-	if area.is_in_group("player"):
-		if bounce_on_hit:
-			var delta = get_process_delta_time() 
-			$KnockTimer.start()
-			set_process(false)
-			while $KnockTimer.time_left > 0:
-				if !get_tree().paused:
-					position.y -= 200 * delta
-				await get_tree().create_timer(0.01 * delta).timeout
+	if area.is_in_group("player") and bounce_on_hit:
+		take_knockback()
 
 func _on_knock_timer_timeout():
 	set_process(true)
@@ -37,10 +48,9 @@ func explode():
 	boom.position = position
 	add_sibling(boom)
 	boom.act(1.5)
-	if drop_heal_item_on_death:
+	if drop_heal_item_on_death and playervars.health < 3:
 		# if the player's health is not full, roll a chance to drop a healing item
-		if playervars.health < 3:
-			if randf_range(0, 12) <= 4:
-				var healitem = sceneheal.instantiate()
-				healitem.position = position
-				call_deferred("add_sibling", healitem)
+		if randf_range(0, 12) <= 4:
+			var healitem = sceneheal.instantiate()
+			healitem.position = position
+			call_deferred("add_sibling", healitem)
--- scenes/entities/projectiles/playerbullet/bullet.gd
+++ scenes/entities/projectiles/playerbullet/bullet.gd
@@ -13,7 +13,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 # TODO: change up this code to account for possible future enemies with extra health and enemies that doesnt explode on the spot
 func _on_area_entered(area):
 	if area.is_in_group("enemy"):
-		area.explode()
+		area.take_damage()
 		area.queue_free()
 		playervars.score += 1
 		queue_free()
